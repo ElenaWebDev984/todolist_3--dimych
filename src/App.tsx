baa0c04bba26cff588f7bd21b1a9290bdebf5f5e
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-13 02:49:51 +0200

    added lesson-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-
 import './App.css'
+import {TaskType, Todolist} from "./Todolist.tsx";
+
 
 function App() {
 
-  return (
-    <>
+    let tasks1: TaskType[] = [
+        {id: 1, title: 'CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+    ]
 
-    </>
-  )
+    let tasks2: TaskType[] = [
+        {id: 1, title: 'Terminator', isDone: true},
+        {id: 2, title: 'XXX', isDone: false},
+        {id: 3, title: 'Jentelmens of fortune', isDone: true},
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'Movies'} tasks={tasks2}/>
+        </div>
+    )
 }
+
 
 export default App
