568e7393c810a342d2bf36d185b3b829bf7b6b45
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-15 01:49:32 +0200

    added useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import './App.css'
 import {TaskType, Todolist} from "./Todolist.tsx";
+import {useState} from "react";
+
+// useState - это функция, которая принимает данные и возвращает массив
+// function useState2(data: any) {
+//     return [[{}, {}, {}], () => {}]
+// }
+//
+// let arr = useState2([{}, {}, {}])
+// let tasks = arr[0];
+// let setTasks = arr[1];
 
 
 function App() {
 
-    let tasks: TaskType[] = [
+    let initTasks: TaskType[] = [
         {id: 1, title: 'CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
         {id: 3, title: 'React', isDone: false},
         {id: 4, title: 'Redux', isDone: false},
     ]
 
+ let arr = useState(initTasks); // initial state
+
+ let tasks = arr[0]
+ let setTasks = arr[1]
+
+
     const removeTask = (id: number) => {
-        tasks = tasks.filter(task => task.id !== id)
+        initTasks = initTasks.filter(task => task.id !== id)
     }
 
     // TODO в новый массив фильтруем/добавляем все таски, id которой не равна id той таски, которую нам надо удалить
